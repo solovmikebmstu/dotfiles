c00eb911209d04a8a3863fa05d1b5c551f2a57bb
solovmikebmstu <solovmikebmstu@gmail.com>
2026-06-21 19:46:29 +0300

    add .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,16 @@
+(global-display-line-numbers-mode 1)
+(setq make-backup-files nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(eglot-inactive-regions)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
